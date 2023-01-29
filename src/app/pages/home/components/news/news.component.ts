b3d4d9d16201b405938e8b1b58276247d779bd01
Sylvain Bousselier <orphen92300@gmail.com>
2023-01-29 16:41:18 +0100

    start news block

--- src/app/pages/home/components/news/news.component.ts
+++ src/app/pages/home/components/news/news.component.ts
@@ -3,6 +3,10 @@ import { News } from '@app/models/backend/components/news';
 import { GlobalService } from '@app/services/global';
 import { Observable } from 'rxjs';
 
+import SwiperCore, { Navigation, Pagination, Autoplay, SwiperOptions } from 'swiper';
+
+SwiperCore.use([Navigation, Pagination, Autoplay]);
+
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
@@ -11,12 +15,34 @@ import { Observable } from 'rxjs';
 export class NewsComponent implements OnInit {
   public datas$: Observable<News>;
   private id: string;
+  public config: SwiperOptions = {
+    autoplay: {delay: 6000, pauseOnMouseEnter:true, disableOnInteraction:false},
+    loop: true,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.customPagination',
+      bulletElement:"div",
+      clickable: true
+     }
+  };
   constructor(
     private globalService: GlobalService
   ) {}
 
   ngOnInit(): void {
     this.datas$ = this.globalService.getDataComponent(this.id);
+  }
+
+  slideNext() {
+
+  }
+
+  slidePrev() {
+
   }
 
 }
