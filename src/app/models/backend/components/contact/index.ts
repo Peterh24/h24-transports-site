d7d0e02d577b44452b86161dccae4181087e7426
Sylvain Bousselier <bousselier@natexo.com>
2023-02-15 17:09:00 +0100

    Auto generate form

--- src/app/models/backend/components/contact/index.ts
+++ src/app/models/backend/components/contact/index.ts
@@ -2,10 +2,25 @@ export interface Contact {
   info: {
     title: string;
   },
-  list: Array<{
-    label: string;
-    type: string;
-    require: boolean;
-  }>;
+  list: Array<JsonFormControls>;
 }
 
+export interface JsonFormControls {
+  name: string;
+  label: string;
+  value: string;
+  type: string;
+  validators: JsonFormValidators;
+}
+
+export interface JsonFormValidators {
+  min?: number;
+  max?: number;
+  required?: boolean;
+  requiredTrue?: boolean;
+  email?: boolean;
+  minLength?: boolean;
+  maxLength?: boolean;
+  pattern?: string;
+  nullValidator?: boolean;
+}
--- src/app/pages/home/components/contact/contact.component.ts
+++ src/app/pages/home/components/contact/contact.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Contact } from '@app/models/backend/components/contact';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Contact, JsonFormControls } from '@app/models/backend/components/contact';
 import { GlobalService } from '@app/services/global';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent {
-  form: FormGroup
+export class ContactComponent implements OnInit  {
+  public form: FormGroup = this.fb.group({});
   public datas$: Observable<Contact>;
   public id: string;
 
@@ -21,34 +22,64 @@ export class ContactComponent {
 
   ngOnInit(): void {
     this.datas$ = this.globalService.getDataComponent(this.id);
-    this.form = this.fb.group(
-    {
-      lastname: [null, {
-        updateOn: 'blur',
-        validators: [
-          Validators.required, Validators.minLength(3)
-        ]
-      }],
-      firstname: [null, {
-        updateOn: 'blur',
-        validators: [
-          Validators.required, Validators.minLength(3)
-        ]
-      }],
-      subject: [null, {
-        updateOn: 'blur',
-        validators: [
-          Validators.required, Validators.minLength(3)
-        ]
-      }],
-      message: [null, {
-        updateOn: 'blur',
-        validators: [
-          Validators.required, Validators.minLength(3)
-        ]
-      }],
-    }
-    );
+
+    this.datas$.pipe(take(1)).subscribe(data => {
+      this.createForm(data.list);
+    })
+  }
+
+  createForm(controls: JsonFormControls[]){
+      for (const control of controls) {
+        const validatorsToAdd:any = [];
+        for (const [key, value] of Object.entries(control.validators)) {
+          switch (key) {
+            case 'min':
+              validatorsToAdd.push(Validators.min(value));
+              break;
+            case 'max':
+              validatorsToAdd.push(Validators.max(value));
+              break;
+            case 'required':
+              if (value) {
+                validatorsToAdd.push(Validators.required);
+              }
+              break;
+            case 'requiredTrue':
+              if (value) {
+                validatorsToAdd.push(Validators.requiredTrue);
+              }
+              break;
+            case 'email':
+              if (value) {
+                validatorsToAdd.push(Validators.email);
+              }
+              break;
+            case 'minLength':
+              validatorsToAdd.push(Validators.minLength(value));
+              break;
+            case 'maxLength':
+              validatorsToAdd.push(Validators.maxLength(value));
+              break;
+            case 'pattern':
+              validatorsToAdd.push(Validators.pattern(value));
+              break;
+            case 'nullValidator':
+              if (value) {
+                validatorsToAdd.push(Validators.nullValidator);
+              }
+              break;
+            default:
+              break;
+          }
+        }
+
+        // Ajoutez des contrôles à la FormGroup
+        this.form.addControl(
+          control.name,
+          new FormControl(control.value, validatorsToAdd)
+        );
+        console.log('form: ', this.form);
+      }
   }
 
   onPatchValue(): void {
